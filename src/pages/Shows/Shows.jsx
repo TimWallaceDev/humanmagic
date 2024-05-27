f0a4c8ce2a05faae2aa4b211d2de3b42714791bd
Tim Wallace <timwallacedev@gmail.com>
2024-05-27 15:10:57 -0400

    update shows page

--- src/pages/Shows/Shows.jsx
+++ src/pages/Shows/Shows.jsx
@@ -10,9 +10,10 @@ export function Shows() {
             <h1 className="shows__heading">Upcoming Shows</h1>
             <hr className="hr hr--top" />
             <div className="shows__content">
-                <Show/>
                 {/* <Show/> */}
-                <hr className="hr hr--show-bottom"/>
+                {/* <hr className="hr hr--show-bottom"/> */}
+                <p>No upcoming shows...</p>
+                <p>Check back soon!</p>
             </div>
             <hr className="hr hr--bottom-top" />
             <hr className="hr hr--bottom-bottom" />
